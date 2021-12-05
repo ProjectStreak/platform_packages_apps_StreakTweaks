503f4c644eda96b788075d21802ea31de04e39f7
acras01 <acras1@gmail.com>
2021-12-05 03:06:12 +0100

    Tweaks: Allow toggling combined signal icons [2/2]
    * @jhonboy121: remove preference controller usaeg and use the default value for switch from sysui resource
    idoybh (YAAP): Good ol' java
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: idoybh <idoybh2@gmail.com>

--- src/com/streak/tweaks/fragments/StatusBarSettings.java
+++ src/com/streak/tweaks/fragments/StatusBarSettings.java
@@ -3,7 +3,6 @@ package com.streak.tweaks.fragments;
 import com.android.internal.logging.nano.MetricsProto;
 
 import android.os.Bundle;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.UserHandle;
@@ -11,12 +10,8 @@ import android.content.ContentResolver;
 import android.content.res.Resources;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
-import androidx.preference.PreferenceGroup;
 import androidx.preference.PreferenceScreen;
-import androidx.preference.PreferenceCategory;
 import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.PreferenceFragment;
-import androidx.preference.SwitchPreference;
 import android.provider.Settings;
 import com.android.settings.R;
 
@@ -28,28 +23,58 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 import android.util.Log;
 
-import java.util.List;
-import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.Collections;
+
+import com.streak.support.preferences.SecureSettingSwitchPreference;
 
 public class StatusBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private static final String SYSTEMUI_PACKAGE = "com.android.systemui";
+    private static final String CONFIG_RESOURCE_NAME = "flag_combined_status_bar_signal_icons";
+    private static final String COBINED_STATUSBAR_ICONS = "show_combined_status_bar_signal_icons";
+
+    private SecureSettingSwitchPreference mCombinedIcons;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-
         addPreferencesFromResource(R.xml.streak_tweaks_statusbar);
-
         PreferenceScreen prefSet = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
 
+        mCombinedIcons = (SecureSettingSwitchPreference)
+                findPreference(COBINED_STATUSBAR_ICONS);
+        Resources sysUIRes = null;
+        boolean def = false;
+        int resId = 0;
+        try {
+            sysUIRes = getActivity().getPackageManager()
+                    .getResourcesForApplication(SYSTEMUI_PACKAGE);
+        } catch (Exception ignored) {
+            // If you don't have system UI you have bigger issues
+        }
+        if (sysUIRes != null) {
+            resId = sysUIRes.getIdentifier(
+                    CONFIG_RESOURCE_NAME, "bool", SYSTEMUI_PACKAGE);
+            if (resId != 0) def = sysUIRes.getBoolean(resId);
+        }
+        boolean enabled = Settings.Secure.getInt(resolver,
+                COBINED_STATUSBAR_ICONS, def ? 1 : 0) == 1;
+        mCombinedIcons.setChecked(enabled);
+        mCombinedIcons.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-
+        final ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mCombinedIcons) {
+            boolean enabled = (boolean) objValue;
+            Settings.Secure.putInt(resolver,
+                    COBINED_STATUSBAR_ICONS, enabled ? 1 : 0);
+            return true;
+        }
         return false;
     }
 
