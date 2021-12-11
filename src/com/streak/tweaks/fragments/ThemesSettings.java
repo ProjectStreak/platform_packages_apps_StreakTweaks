f1dc9bf118e2df00a37f6918883a770b23570c47
spkal01 <kalligeross@gmail.com>
2021-12-11 16:43:52 +0100

    Tweaks: Import font and icon pack theming

--- src/com/streak/tweaks/fragments/ThemesSettings.java
+++ src/com/streak/tweaks/fragments/ThemesSettings.java
@@ -13,34 +13,68 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.SystemProperties;
 import android.os.UserHandle;
+import androidx.fragment.app.Fragment;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
 import android.provider.Settings;
-import com.android.settings.R;
 
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.development.OverlayCategoryPreferenceController;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Map;
 
-import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.development.OverlayCategoryPreferenceController;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
 
-public class ThemesSettings extends SettingsPreferenceFragment implements
+public class ThemesSettings extends DashboardFragment implements
         OnPreferenceChangeListener {
+
+    public static final String TAG = "ThemesSettings";
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+    }
 
-        addPreferencesFromResource(R.xml.streak_tweaks_themes);
-
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.streak_tweaks_themes;
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-
         return false;
+    }
+
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, getSettingsLifecycle(), this);
+    }
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(
+            Context context, Lifecycle lifecycle, Fragment fragment) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.font"));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.icon_pack"));
+        return controllers;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
     }
 
     @Override
